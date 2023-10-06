cf3433a1ee10b3a958ab21b564563190c855219e
viviansdrv <83466012+viviansdrv@users.noreply.github.com>
2023-10-05 21:41:14 -0300

    uso de api pokeapi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log("Sucesso!!")
+// https://pokeapi.co/api/v2/pokemon?offset=0&limit=10
+
+const offset = 0
+const limit = 20
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    /* .then(function (response){
+            return response.json()
+    }) mesma coisa que a de baixo*/
+    
+    .then ((response) => response.json()) // quando a funçao tiver apenas um comando, pode reduzir em uma linha
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('Requisição Concluída!'))
+

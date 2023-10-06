407963cf9a16946d8ff295ba2733056f4bc05c50
viviansdrv <83466012+viviansdrv@users.noreply.github.com>
2023-10-06 01:08:25 -0300

    nome e url adicionados

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,37 @@ const offset = 0
 const limit = 20
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                    </div>
+        </li>
+        ` 
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     /* .then(function (response){
             return response.json()
     }) mesma coisa que a de baixo*/
     
-    .then ((response) => response.json()) // quando a funçao tiver apenas um comando, pode reduzir em uma linha
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.log(error))
-    .finally(() => console.log('Requisição Concluída!'))
+    .then ((response) => response.json()) 
+    .then((jsonBody) => jsonBody.results)
+    //.then((pokemonList) => console.log(pokemonList[0]))
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+
+        }
+    })
 
